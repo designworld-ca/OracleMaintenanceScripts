2dd914025172494364416517d678cefa6d3adadb
designworld-ca <47788179+designworld-ca@users.noreply.github.com>
2019-07-10 09:15:52 -0400

    Create ORA-01194: file 1 needs more recovery to be consistent.sql

---
+++ ORA-01194: file 1 needs more recovery to be consistent.sql
@@ -0,0 +1,72 @@
+ORA-01194: file 1 needs more recovery to be consistent
+
+
+SQL> alter database open resetlogs;
+alter database open resetlogs
+*
+ERROR at line 1:
+ORA-01194: file 1 needs more recovery to be consistent
+ORA-01110: data file 1: '/u01/app/oracle/oradata/RTS_NEW/system_new.dbf'
+Workaround for this error is to provide all the available archive log files to the recovery:
+
+SQL> recover database using backup controlfile until cancel;
+...
+Specify log: {<RET>=suggested | filename | AUTO | CANCEL}
+AUTO
+Above command will apply all the available archive logs automatically. Now try to open database with resetlogs:
+
+SQL> alter database open resetlogs;
+If the error persists due to insufficient archive logs, do the following workaround:
+
+SQL> shutdown immediate
+
+ORA-01109: database not open
+Database dismounted.
+ORACLE instance shut down.
+Startup database in mount mode:
+
+SQL> startup mount
+
+ORACLE instance started.
+Total System Global Area 530288640 bytes
+Fixed Size 2131120 bytes
+Variable Size 310381392 bytes
+Database Buffers 209715200 bytes
+Redo Buffers 8060928 bytes
+Database mounted.
+Change "_allow_resetlogs_corruption" parameter to TRUE and undo_management parameter to MANUAL:
+
+SQL> ALTER SYSTEM SET "_allow_resetlogs_corruption"= TRUE SCOPE = SPFILE;
+SQL> ALTER SYSTEM SET undo_management=MANUAL SCOPE = SPFILE;
+After doing above changes, shutdown database, and startup:
+
+SQL> shutdown immediate
+ORA-01109: database not open
+Database dismounted.
+ORACLE instance shut down.
+SQL> startup mount
+ORACLE instance started.
+Total System Global Area 530288640 bytes
+Fixed Size 2131120 bytes
+Variable Size 310381392 bytes
+Database Buffers 209715200 bytes
+Redo Buffers 8060928 bytes
+Database mounted.
+Now try resetlogs:
+
+SQL> alter database open resetlogs;
+May crash, if so, mount and alter database open resetlogs or just alter database open
+
+Database altered.
+Create new undo tablespace and set “undo_tablespace” parameter to the new undo tablespace and change “undo_management” parameter to AUTO:
+
+SQL> CREATE UNDO TABLESPACE undo2 datafile '/u01/app/oracle/oradata/RTS_NEW/undo2_df1.dbf' size 200m autoextend on maxsize 30G;
+Tablespace created.
+SQL> alter system set undo_tablespace = undo2 scope=spfile;
+System altered.
+SQL> alter system set undo_management=auto scope=spfile;
+System altered.
+Now bounce your database.
+
+SQL> shutdown immediate
+SQL> startup
