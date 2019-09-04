7dd94c690cb10406ee3d7cb556150eed3fc8bb3d
designworld-ca <47788179+designworld-ca@users.noreply.github.com>
2019-09-04 09:49:20 -0400

    Create DropADatabase.sql

---
+++ DropADatabase.sql
@@ -0,0 +1,18 @@
+set oracle_sid=<dbName>
+sqlplus / as sysdba
+shutdown immediate
+startup mount exclusive restrict
+exit
+rman 
+connect target sys
+
+drop database including backups noprompt;
+exit
+
+--delete empty folders
+--delete admin trace folders
+--run
+--on windows
+sc delete <service name>
+
+--delete from tnsnames and listener.ora
