f93a25430ccec5a93d254d2e92677411a87579f0
kevinsky <47788179+designworld-ca@users.noreply.github.com>
2020-01-17 09:19:31 -0500

    Create CheckBackupsUsingRman.sql

---
+++ RMAN/CheckBackupsUsingRman.sql
@@ -0,0 +1,11 @@
+LIST BACKUP;
+
+LIST BACKUP OF ARCHIVELOG ALL;
+
+LIST BACKUP OF CONTROLFILE;
+
+LIST BACKUP OF SPFILE;
+
+--if you get errors regarding incarnations of the database
+
+LIST INCARNATION;
