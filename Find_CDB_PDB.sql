d2ce961ef247b96cec516da800a676d7ab32c997
kevinsky <47788179+designworld-ca@users.noreply.github.com>
2020-03-12 08:32:45 -0400

    Update Find_CDB_PDB.sql

--- Find_CDB_PDB.sql
+++ Find_CDB_PDB.sql
@@ -1,4 +1,6 @@
 --shows whether you are in a cdb or pdb
+column DB_NAME format a12
+column TYPE format a10
 
 select decode(sys_context('USERENV', 'CON_NAME'),'CDB$ROOT',sys_context('USERENV', 'DB_NAME'),sys_context('USERENV', 'CON_NAME')) DB_NAME,
             decode(sys_context('USERENV','CON_ID'),1,'CDB','PDB') TYPE 
